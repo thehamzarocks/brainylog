8dee05942c355ae037b61b7a29d595183babca77
thehamzarocks <thehamzarocks@gmail.com>
2020-06-27 11:00:18 +0530

    writing simple info logs to file

--- brainyLog.go
+++ brainyLog.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func processAddArgs(args []string) (string, string) {
@@ -41,6 +45,36 @@ func showBrainyLogVersion() {
 }
 
 func addBrainyLogLog(logType string, log string) {
-	fmt.Println(logType)
-	fmt.Println(log)
+	filename := "build/brainyLog.bl"
+	line := processLine(logType, log)
+	fmt.Println(filename, line)
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	if _, err = f.WriteString(line + "\n"); err != nil {
+		panic(err)
+	}
+}
+
+func processLine(logtype string, log string) string {
+	t := time.Now()
+	timestamp := t.Format(time.RFC3339)
+	epoch := t.UnixNano() / 1e6
+
+	var str strings.Builder
+	str.WriteString("(")
+	str.WriteString(timestamp)
+	str.WriteString(")")
+	str.WriteString("[")
+	str.WriteString(strconv.FormatInt(epoch, 10))
+	str.WriteString(".0")
+	str.WriteString("]")
+	str.WriteString(uuid.New().String())
+	str.WriteString(">")
+	str.WriteString(log)
+
+	return str.String()
 }
